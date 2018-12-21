8b7bb530029765a9886bc6881f916168f032d614
Edilson do Nascimento <edilsonjoper@gmail.com>
2018-12-21 19:05:13 -0200

    12. Criado Escopo de Sessão para a classe componente Carrinho e Escopo de Request para classe Controller

--- src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
