cc878e13bfe7fdd874df87db3d2983ed11975ce4
Edilson do Nascimento <edilsonjoper@gmail.com>
2018-12-28 20:18:32 -0200

    2 - 09: 02 - Deploy no Heroku

--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -15,7 +15,8 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {SecurityConfiguration.class,AppWebConfiguration.class, JPAConfiguration.class};
+		return new Class[] {SecurityConfiguration.class,AppWebConfiguration.class, 
+				JPAConfiguration.class, JPAProdutctionConfiguration.class};
 	}
 
 	@Override
@@ -41,12 +42,12 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 		registration.setMultipartConfig(new MultipartConfigElement(""));
 	}
 	
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		super.onStartup(servletContext);
-		servletContext.addListener(new RequestContextListener());
-		servletContext.setInitParameter("spring.profiles.active", "dev");
-
-	}
+//	@Override
+//	public void onStartup(ServletContext servletContext) throws ServletException {
+//		super.onStartup(servletContext);
+//		servletContext.addListener(new RequestContextListener());
+//		servletContext.setInitParameter("spring.profiles.active", "dev");
+//
+//	}
 	
 }
